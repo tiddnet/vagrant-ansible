7ce12faa9eea63140164ca6485fdfce1fe36f3bf
Sher Chowdhury <sher.chowdhury@os.uk>
2016-02-01 15:53:25 +0000

    written ansible install script

---
+++ scripts/install-ansible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git clone git://github.com/ansible/ansible.git --recursive
+
+cd ansible
+git checkout tags/v2.0.0.1-1
+
+source ./hacking/env-setup    # I think this needs to be added in the bashrc profile file. 
+
+echo "source /root/ansible/hacking/env-setup 1>/dev/null 2>&1" >> /root/.bashrc
+
+easy_install pip
+
+yum install python-devel -y     # this package is in the "base" yum repo
+
+pip install paramiko PyYAML Jinja2 httplib2 six
+
+
+ansible --version
