e8b4aac617b5aaf19804f162fd75f57fc1c49b98
Sher <sher.chowdhury@gmail.com>
2016-02-03 22:31:42 +0000

    lots of refactoring and bug fixes.

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+echo "INFO: About to run scripts/install-ansible.sh"
+yum install python-devel -y  || exit 1     # this package is in the "base" yum repo
+yum install python2-crypto -y  || exit 1
+yum install asciidoc -y  || exit 1
+yum install rpm-build -y  || exit 1
+
+cd /tmp ||  exit 1
+git clone git://github.com/ansible/ansible.git --recursive  || exit 1
+cd ansible  || exit 1
+make rpm  || exit 1
+git checkout tags/v2.0.0.1-1  || exit 1
+cd rpm-build/  || exit 1
+yum localinstall ansible-*.noarch.rpm -y  || exit 1
+
+echo "line 14"
+echo "INFO: Ansible has been successfully installed"
+yum info ansible  || exit 1
+exit 0
+
+
+# Ignore the rest, it's no longer required.
 
 useradd ansible
 echo "vagrant" | passwd --stdin ansible
--- scripts/setup-ansible-controller-ssh-keys.sh
+++ scripts/setup-ansible-controller-ssh-keys.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 echo "INFO: About to run setup-ansible-controller-ssh-keys.sh"
 
-cp /vagrant/files/ssh-keys/controller/.ssh /home/ansible/ -rf
+cp /vagrant/files/ssh-keys/controller/.ssh /root/ -rf
 
-chown --recursive ansible:ansible /home/ansible/.ssh
+chown --recursive root:root /root/.ssh
 
-chmod 700 /home/ansible/.ssh
-
-chmod 600 /home/ansible/.ssh/id_rsa
-chmod 644 /home/ansible/.ssh/id_rsa.pub
-chmod 644 /home/ansible/.ssh/known_hosts
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/id_rsa
+chmod 644 /root/.ssh/id_rsa.pub
+# chmod 644 /root/.ssh/known_hosts
 
 # this disables RSA fingerprint checking when connecting to a new host. 
 echo "
 Host *
     StrictHostKeyChecking no
-" > /home/vagrant/.ssh/config
+" > /root/.ssh/config
