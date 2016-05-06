aea461913bce296230ff5dcc0e9e3c27d3b0cf06
Sher <sher.chowdhury@gmail.com>
2016-05-06 11:42:33 +0100

    refactoring

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -11,36 +11,14 @@ cd /tmp ||  exit 1
 git clone git://github.com/ansible/ansible.git --recursive  || exit 1
 cd ansible  || exit 1
 make rpm  || exit 1
-git checkout tags/v2.0.0.1-1  || exit 1
+#git checkout tags/v2.0.0.1-1  || exit 1
 cd rpm-build/  || exit 1
 yum localinstall ansible-*.noarch.rpm -y  || exit 1
 
 echo "line 14"
 echo "INFO: Ansible has been successfully installed"
 yum info ansible  || exit 1
+
 exit 0
 
 
-# Ignore the rest, it's no longer required.
-
-useradd ansible
-echo "vagrant" | passwd --stdin ansible
-
-runuser -l ansible -c 'git clone git://github.com/ansible/ansible.git --recursive'  
-
-runuser -l ansible -c 'cd ansible'
-
-runuser -l ansible -c 'git checkout tags/v2.0.0.1-1'
-
-runuser -l ansible -c 'source ./hacking/env-setup '
-
-runuser -l ansible -c 'echo "source /home/ansible/ansible/hacking/env-setup 1>/dev/null 2>&1" >> /home/ansible/.bashrc'
-
-
-easy_install pip
-
-yum install python-devel -y     # this package is in the "base" yum repo
-
-pip install paramiko PyYAML Jinja2 httplib2 six
-
-runuser -l ansible -c 'ansible --version'
